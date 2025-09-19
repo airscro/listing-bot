0fcd6311e1ec23d22e8bb91220d2c4def91eef41
Airs <skibidisigmanic@gmail.com>
2025-09-19 12:22:17 +0500

    Remove config.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import discord
 from discord.ext import commands
 import os
 from config import BOT
-from components.start import StartExchange
 from utils.status import status, activity
 
 intents = discord.Intents.all()
@@ -27,7 +26,7 @@ def load_extensions():
         if filename.endswith(".py"):
             bot.load_extension(f"commands.{filename[:-3]}")
             print(f"Loaded - {filename[:-3]}")
-    bot.load_extension("utils.restore") # remove this if your bot doesn't have any components
+    # bot.load_extension("utils.restore") # remove this if your bot doesn't have any components
 
 
 if __name__ == "__main__":
